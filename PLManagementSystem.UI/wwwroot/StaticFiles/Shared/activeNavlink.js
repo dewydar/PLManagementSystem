a0563956359264f950ea4932d1477554e4446f3c
Mostafa Dewydar <mdewydar@sans.com.sa>
2024-07-31 22:21:59 +0300

    finish : user

--- PLManagementSystem.UI/wwwroot/StaticFiles/Shared/activeNavlink.js
+++ PLManagementSystem.UI/wwwroot/StaticFiles/Shared/activeNavlink.js
@@ -1,11 +1,29 @@
-//script to set nav-link to be active according to current pathname(URL)
-document.addEventListener('DOMContentLoaded', () => {
-    var url = location.pathname
-    document.querySelectorAll('.nav-link').forEach(link => {
-        if (link.getAttribute('href').toLowerCase() === url.toLowerCase() || link.getAttribute('href').toLowerCase() === "/" + url.split("/")[1].toLowerCase()) {
-            link.classList.add('active');
-        } else {
-            link.classList.remove('active');
+var ActiveLayoutLink = function () {
+    var currentPath = window.location.pathname.substring(1);
+    var currentPathSplit = currentPath.split('/').filter(function (v) { return v !== '' });
+    var controllerName = currentPathSplit[0];
+    var itemActivemain = [];
+    $('.nav-link').each(function () {
+        var sideLink = $(this).attr('href');
+        var sideLinkSplit = sideLink.split('/').filter(function (v) { return v !== '' });
+        var currentlinkController = sideLinkSplit[0];
+        if (sideLink === "/" + currentPath) {
+            $(this).addClass('active');
+            this.parentElement.parentElement.parentElement.classList.add('menu-is-opening', 'menu-open');
         }
-    });
-})
+        else if (currentlinkController == controllerName) {
+            itemActivemain.push(this);
+        }
+    })
+    if ($('.nav-link').hasClass('active') == false) {
+        itemActivemain.filter((u) => {
+            var href = $(u).attr('href');
+            var sideLinkSplit = href.split('/').filter(function (v) { return v !== '' });
+            var currentlinkAction = sideLinkSplit[1];
+            if (currentlinkAction == undefined) {
+                $(u).addClass('active');
+                u.parentElement.parentElement.parentElement.classList.add('menu-is-opening', 'menu-open');
+            }
+        });
+    }
+}
